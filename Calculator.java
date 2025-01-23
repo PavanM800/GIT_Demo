6491b9c5c63e67dcd1159f92fb402f12ec440bae
PavanM800 <pvmitragotri@git.edu>
2025-01-23 06:20:51 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9;
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(2, 3));
+        System.out.println("Subtraction: " + calc.subtract(5, 2));
+    }
+}
